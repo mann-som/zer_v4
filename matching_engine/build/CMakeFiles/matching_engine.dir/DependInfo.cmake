b93125a1cb76668227e0471dd2a6883d5feabcc9
Mann <mannsomofficial@gmail.com>
2026-01-13 17:33:38 +0530

    c++ matching engine basic - tested manually

---
+++ matching_engine/build/CMakeFiles/matching_engine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anush/Desktop/Mann_codes/prep/ZER_V4/matching_engine/src/engine/order_book.cpp" "/home/anush/Desktop/Mann_codes/prep/ZER_V4/matching_engine/build/CMakeFiles/matching_engine.dir/src/engine/order_book.cpp.o"
+  "/home/anush/Desktop/Mann_codes/prep/ZER_V4/matching_engine/src/main.cpp" "/home/anush/Desktop/Mann_codes/prep/ZER_V4/matching_engine/build/CMakeFiles/matching_engine.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
